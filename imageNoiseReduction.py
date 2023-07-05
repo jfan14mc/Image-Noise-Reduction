9f5d9208370ce9b58368cbba642f9be9b503322b
jfan14mc <129296779+jfan14mc@users.noreply.github.com>
2023-07-05 09:25:46 -0400

    Able to process multiple materials in one run

--- imageNoiseReduction.py
+++ imageNoiseReduction.py
@@ -15,38 +15,62 @@ def export(array, string):
 # stacks the a list of images into a single array and then divides them by the number of images to average them.
 def stack(list):
      return (np.add.reduce(np.array(list).astype(np.uint16))/len(list))
+
+
+def materialList(search):
+     materials = []
+     addresses = glob.glob(search)
+     for address in addresses:
+          materials.append(address)
+     return materials
      
 
-# Category folder to be searched through
-path = input('Enter the category folder path (format is ...Sand Images/MaterialName-Condition):')
-
-# Loop that finds the directoy of all the image forlders inside the category folder
-for wavelength in next(os.walk(path))[1]:
-    wave_dir = os.path.join(path, wavelength)
-
-    # Setting up the file pathing to for result files, Wet Sand(dirname)/Composites/MaterialType-Condition(basename)/wavelength
-    pre_path = os.path.join(os.path.dirname(path), 'Composites', wavelength)
+base = input("Enter the path to the Sand Images Folder:")
+materials = []
+item = input("Enter a material to be composited, if no more to add enter 'None':")
+while item != "None":
+    materials.append(item)
+    item = input("Enter a material to be composited:")
     
-    # Prepares a list for the pictures to be stored in and then unloads all the pictures in an image folder into it
-    pictures = []
-    pictures.clear()
-    for picture in glob.glob(wave_dir + '/*'):
-        pictures.append(np.array(PIL.Image.open(picture)).astype(np.uint16))
 
-    # finds all images in the folder, then stacks and averages files w/ type conversion to prevent overflows
-    imageArrays = [picture for picture in pictures if (picture.sum()/picture.size) > 10]
-    imageStack = stack(imageArrays)
+materialPaths = []
+for material in materials:
+    searchPath = os.path.join(base, '*' + material + '*')
+    materialPaths.extend(materialList(searchPath))
 
-    # stacks and averages all static images, and if static images were present, it subtracts the static average from theimage average
-    # tags the filename if subtraction occured.
-    staticArrays = [picture for picture in pictures if (picture.sum()/picture.size) < 10]
-    if len(staticArrays) > 0:
-        staticStack = stack(staticArrays)
-        resultStack = imageStack - staticStack
-        fileName = pre_path + '-.tiff' 
-        export(resultStack, fileName)
-    else:
-         fileName = pre_path + '.tiff'
-         export(imageStack, fileName)
+for material in materialPaths:
+    combName = os.path.basename(material)
+    composite = os.path.join(base, 'Composites', combName)
+    if os.path.exists(composite) == False:
+        os.mkdir(composite)
 
-    print(fileName)
+    # Loop that finds the directoy of all the image folders inside the category folder
+    for wavelength in next(os.walk(material))[1]:
+        wave_dir = os.path.join(base, combName, wavelength)
+
+        # Setting up the file pathing to for result files, Wet Sand(dirname)/Composites/MaterialType-Condition(basename)/wavelength
+        pre_path = os.path.join(composite, wavelength)
+        
+        # Prepares a list for the pictures to be stored in and then unloads all the pictures in an image folder into it
+        pictures = []
+        pictures.clear()
+        for picture in glob.glob(os.path.join(wave_dir, '*')):
+            pictures.append(np.array(PIL.Image.open(picture)).astype(np.uint16))
+
+        # finds all images in the folder, then stacks and averages files w/ type conversion to prevent overflows
+        imageArrays = [picture for picture in pictures if (picture.sum()/picture.size) > 10]
+        imageStack = stack(imageArrays)
+
+        # stacks and averages all static images, and if static images were present, it subtracts the static average from theimage average
+        # tags the filename if subtraction occured.
+        staticArrays = [picture for picture in pictures if (picture.sum()/picture.size) < 10]
+        if len(staticArrays) > 0:
+            staticStack = stack(staticArrays)
+            resultStack = imageStack - staticStack
+            fileName = pre_path + '-.tiff' 
+            export(resultStack, fileName)
+        else:
+            fileName = pre_path + '.tiff'
+            export(imageStack, fileName)
+
+        print(fileName)
